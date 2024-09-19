4e3d65e502815dfccb696f202142a04b068aa9be
remixxx31 <remi.perray@epitech.eu>
2024-09-19 14:08:14 +0200

    feat(setup archi):Setup project, Add Flutter project files for Linux, iOS, macOS, Android, and Windows Setting up an architecture #91

---
+++ mobile/lib/app.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final FlutterTts flutterTts = FlutterTts();
+  List<String> shoppingList = ['MELON', 'POMME', 'PÂTES'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shop4Me'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              // TODO: Implement search functionality
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: shoppingList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(shoppingList[index]),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      setState(() {
+                        shoppingList.removeAt(index);
+                      });
+                      _speak('Item supprimé');
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    //TODO: Implement add product functionality
+                    _speak('Ajouter un produit');
+                  },
+                  child: Icon(Icons.add),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // TODO: Implement start shopping functionality
+                    _speak('Commencer les courses');
+                  },
+                  child: Icon(Icons.play_arrow),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _speak(String text) async {
+    await flutterTts.setLanguage("fr-FR");
+    await flutterTts.speak(text);
+  }
+}
