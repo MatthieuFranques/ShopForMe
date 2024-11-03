e6c58aa09969524da23ca02f82d86a2cca730bd5
remixxx <rperray@noous.co>
2024-11-04 00:27:54 +0100

    refactor(models): refactor Product and Shop models
    - Add @HiveType annotation to Product and Shop models
    - Add @HiveField annotations to model properties
    - Generate product.g.dart and shop.g.dart files for Hive serialization
    - Update constructors and fromJson methods for Product and Shop models
    - Remove unused imports in models

--- mobile/lib/models/product.dart
+++ mobile/lib/models/product.dart
@@ -1,7 +1,19 @@
-class Product {
+import 'package:hive/hive.dart';
+
+part 'product.g.dart';
+
+@HiveType(typeId: 2)
+class Product extends HiveObject {
+  @HiveField(0)
   final String id;
+
+  @HiveField(1)
   final String name;
+
+  @HiveField(2)
   final String category;
+
+  @HiveField(3)
   final List<int> layout;
 
   Product({
@@ -13,9 +25,9 @@ class Product {
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
-      id: json['id']?.toString() ?? 'unknown', 
-      name: json['name'] ?? 'Unnamed Product',   
-      category: json['category'] ?? 'Misc',     
+      id: json['id']?.toString() ?? 'unknown',
+      name: json['name'] ?? 'Unnamed Product',
+      category: json['category'] ?? 'Misc',
       layout: List<int>.from(json['layout'] ?? [0, 0]),
     );
   }
