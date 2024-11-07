78c62f881eeb96dccbf65a12a152c01275ad4d4e
Matthieu <matthieufranques@gmail.com>
2024-11-07 11:56:36 +0100

    Add : Script location functionnal need to refacto

--- mobile/lib/services/location_service.dart
+++ mobile/lib/services/location_service.dart
@@ -1,87 +1,243 @@
-import 'dart:convert';
-import 'dart:async';
-import 'dart:math';
 import 'dart:collection';
+import 'dart:convert';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:collection/collection.dart';
+import 'package:mobile/models/Grid.dart';
+import 'package:mobile/models/node.dart'; // Import de la bibliothèque collection
 
-class LocationService {
-  final String apiKey;
+// TODO
+// class LocationService {
 
-  LocationService(this.apiKey);
+double distance1 = 0.0;
+double distance2 = 0.0;
+double distance3 = 0.0;
 
-  static const esp1 = {
-    "id": "ESP01",
-    "layout": [1, 3]
-  };
+Future<Grid> loadGridFromJson(String jsonFilePath) async {
+  final String response = await rootBundle.loadString(jsonFilePath);
+  final List<dynamic> jsonData = jsonDecode(response);
 
-  static const esp2 = {
-    "id": "ESP02",
-    "layout": [15, 25]
-  };
+  List<List<int>> grid = List.generate(jsonData.length,
+      (_) => List<int>.filled(jsonData[0].length, 1)); // Grille initiale
 
-  static const esp3 = {
-    "id": "ESP03",
-    "layout": [7, 36]
-  };
+  List<List<int>> beaconPositions = [];
 
-  static const product_test = {
-    "name": "Brocolie",
-    "layout": [7, 36]
-  };
-  // TODO Test : Qui renvoie bien la bonne position
-  //
-  // Fonction pour trianguler les données
-  Future<List<double>> triangulateData(Map<String, dynamic> esp1,
-      Map<String, dynamic> esp2, Map<String, dynamic> esp3) async {
-    // Positions  ESP
-    final double x1 = esp1['layout'][0];
-    final double y1 = esp1['layout'][1];
-    final double x2 = esp2['layout'][0];
-    final double y2 = esp2['layout'][1];
-    final double x3 = esp3['layout'][0];
-    final double y3 = esp3['layout'][1];
+  for (int rowIndex = 0; rowIndex < jsonData.length; rowIndex++) {
+    List<dynamic> row = jsonData[rowIndex];
+    for (int colIndex = 0; colIndex < row.length; colIndex++) {
+      Map<String, dynamic> cell = row[colIndex];
+      if (cell['type'] == "VIDE") {
+        grid[rowIndex][colIndex] = 0; // Accessible
+      } else {
+        grid[rowIndex][colIndex] = 1; // Inaccessible
+      }
+      if (cell['isBeacon'] == true) {
+        beaconPositions.add([rowIndex, colIndex]);
+      }
+    }
+  }
+  return Grid(jsonData.length, jsonData[0].length, grid);
+}
 
-    // Range ESP
-    final double r1 = esp1['distance'];
-    final double r2 = esp2['distance'];
-    final double r3 = esp3['distance'];
+Future<void> loadDistances(String jsonFilePath) async {
+  final String jsonString = await rootBundle.loadString(jsonFilePath);
+  final List<dynamic> jsonData = jsonDecode(jsonString);
 
-    // Calculate the coefficients for the linear equations
-    final double A =
-        2 * x2 - 2 * x1; // Difference in x-coordinates between ESP2 and ESP1
-    final double B =
-        2 * y2 - 2 * y1; // Difference in y-coordinates between ESP2 and ESP1
-    final double C = r1 * r1 -
-        r2 * r2 -
-        x1 * x1 +
-        x2 * x2 -
-        y1 * y1 +
-        y2 * y2; // Combination of distances and coordinates for ESP1 and ESP2
-
-    final double D =
-        2 * x3 - 2 * x2; // Difference in x-coordinates between ESP3 and ESP2
-    final double E =
-        2 * y3 - 2 * y2; // Difference in y-coordinates between ESP3 and ESP2
-    final double F = r2 * r2 -
-        r3 * r3 -
-        x2 * x2 +
-        x3 * x3 -
-        y2 * y2 +
-        y3 * y3; // Combination of distances and coordinates for ESP2 and ESP3
-
-// Solve the linear equations to find the x-coordinate of the target position
-    final double x = (C * E - F * B) / (E * A - B * D);
-
-// Solve the linear equations to find the y-coordinate of the target position
-    final double y = (C * D - A * F) / (B * D - A * E);
-
-    return [x, y];
+  if (jsonData.length >= 3) {
+    distance1 = jsonData[0]['distance'];
+    distance2 = jsonData[1]['distance'];
+    distance3 = jsonData[2]['distance'];
+  } else {
+    throw Exception('Pas assez de beacons dans le fichier JSON.');
   }
 }
 
-// Mouvement possible dans les 4 directions: Haut, Bas, Gauche, Droite
-final List<List<int>> directions = [
-  [0, 1], // Droite
-  [1, 0], // Bas
-  [0, -1], // Gauche
-  [-1, 0], // Haut
-];
+Future<List<List<int>>> getBeaconPositions(String jsonFilePath) async {
+  final String response = await rootBundle.loadString(jsonFilePath);
+  final List<dynamic> jsonData = jsonDecode(response);
+
+  List<List<int>> beaconPositions = [];
+
+  for (int rowIndex = 0; rowIndex < jsonData.length; rowIndex++) {
+    List<dynamic> row = jsonData[rowIndex];
+    for (int colIndex = 0; colIndex < row.length; colIndex++) {
+      Map<String, dynamic> cell = row[colIndex];
+      if (cell['isBeacon'] == true) {
+        beaconPositions.add([rowIndex, colIndex]);
+      }
+    }
+  }
+
+  return beaconPositions;
+}
+
+List<List<int>> findShortestPath(Grid grid, List<int> start, List<int> goal) {
+  int n = grid.n;
+  int m = grid.m;
+  List<List<int>> distances =
+      List.generate(n, (_) => List<int>.filled(m, 999999));
+  List<List<int>> previous = List.generate(n, (_) => List<int>.filled(m, -1));
+  PriorityQueue<Node> queue = PriorityQueue<Node>();
+
+  distances[start[0]][start[1]] = 0;
+  queue.add(Node(start[0], start[1], 0));
+
+  List<List<int>> directions = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0]
+  ];
+
+  while (queue.isNotEmpty) {
+    Node current = queue.removeFirst();
+    int x = current.x;
+    int y = current.y;
+
+    if (x == goal[0] && y == goal[1]) {
+      break;
+    }
+
+    for (List<int> direction in directions) {
+      int newX = x + direction[0];
+      int newY = y + direction[1];
+
+      if (grid.isValid(newX, newY)) {
+        int newDist = distances[x][y] + 1;
+
+        if (newDist < distances[newX][newY]) {
+          distances[newX][newY] = newDist;
+          previous[newX][newY] = x * m + y;
+          queue.add(Node(newX, newY, newDist));
+        }
+      }
+    }
+  }
+
+  List<List<int>> path = [];
+  int x = goal[0];
+  int y = goal[1];
+
+  while (x != start[0] || y != start[1]) {
+    path.add([x, y]);
+    int prev = previous[x][y];
+    x = prev ~/ m;
+    y = prev % m;
+  }
+
+  path.add(start);
+  path = path.reversed.toList();
+
+  return path;
+}
+
+Future<List<List<int>>?> findTargetPosition() async {
+  String jsonFilePath = '../../assets/plan.json';
+  String jsonDistanceFile = '../../assets/beacon.json';
+  Grid grid = await loadGridFromJson(jsonFilePath);
+
+  List<List<int>> beaconPositions = await getBeaconPositions(jsonFilePath);
+
+  await loadDistances(jsonDistanceFile);
+
+  if (beaconPositions.length >= 3) {
+    List<double> targetPosition = await triangulateData(
+      beaconPositions[0],
+      distance1,
+      beaconPositions[1],
+      distance2,
+      beaconPositions[2],
+      distance3,
+    );
+    //Position actuelle
+    List<int> currentPosition = [
+      targetPosition[0].round(),
+      targetPosition[1].round()
+    ];
+
+    // TODO Position of product
+    List<int> end = [5, 8];
+
+    print(
+        "distance1 : $distance1 , distance2 : $distance2 , distance3: $distance3");
+    print("beaconPositions : $beaconPositions");
+    print("currentPosition : $currentPosition");
+    print("End : $end");
+
+    List<List<int>> path = findShortestPath(grid, currentPosition, end);
+    print("Chemin le plus court : $path");
+    return path;
+  } else {
+    print("Pas assez de beacons pour la triangulation.");
+    return null;
+  }
+}
+
+Future<List<double>> triangulateData(List<int> pos1, double r1, List<int> pos2,
+    double r2, List<int> pos3, double r3) async {
+  final double x1 = pos1[0].toDouble();
+  final double y1 = pos1[1].toDouble();
+  final double x2 = pos2[0].toDouble();
+  final double y2 = pos2[1].toDouble();
+  final double x3 = pos3[0].toDouble();
+  final double y3 = pos3[1].toDouble();
+
+  final double d1 = r1;
+  final double d2 = r2;
+  final double d3 = r3;
+
+  final double A = 2 * (x2 - x1);
+  final double B = 2 * (y2 - y1);
+  final double C = d1 * d1 - d2 * d2 - x1 * x1 + x2 * x2 - y1 * y1 + y2 * y2;
+
+  final double D = 2 * (x3 - x2);
+  final double E = 2 * (y3 - y2);
+  final double F = d2 * d2 - d3 * d3 - x2 * x2 + x3 * x3 - y2 * y2 + y3 * y3;
+
+  final double denominator = (E * A - B * D);
+  if (denominator == 0) {
+    throw Exception('Division par zéro détectée dans la triangulation.');
+  }
+
+  final double x = (C * E - F * B) / denominator;
+  final double y = (C * D - A * F) / (B * D - A * E);
+
+  return [x, y];
+}
+// TODO uncoment with location_service
+// }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    List<List<int>>? path = await findTargetPosition();
+    if (path == null) {
+      print("Aucun chemin trouvé.");
+    } else {
+      print("Le chemin trouvé est : $path");
+    }
+  } catch (e) {
+    print("Une erreur s'est produite : $e");
+  }
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mon Application',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Test de la Fonction'),
+        ),
+        body: Center(
+          child: Text('Vérifiez la console pour les résultats.'),
+        ),
+      ),
+    );
+  }
+}
