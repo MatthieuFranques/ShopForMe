107b8acb6811d4c0fbf713aeb6a0583d1af0b85a
Matthieu <matthieufranques@gmail.com>
2024-11-14 15:29:17 +0100

    Refactor : location_service + add comment inf navigation_bloc

---
+++ mobile/lib/services/location_product_service.dart
@@ -0,0 +1,5 @@
+class LocationProductService {
+  // TODO fonction qui a en param un produit
+  // doit renvoyer ca position.
+  // Regarde dans le json ou es ce que ce qu'il ce trouve dans la matrice [0,1]
+}
