29028b1475360dc0247acd291a56e9479df90b2a
Frederic LE DOUARIN <frederic.ledouarin1@gmail.com>
2024-12-05 21:22:00 +0100

    fix: fixing double scan connections

--- mobile/lib/blocs/navigation_bloc.dart
+++ mobile/lib/blocs/navigation_bloc.dart
@@ -1,53 +1,54 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
+import 'dart:io';
+import 'dart:isolate';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:mobile/services/bluetooth_service.dart';
 import 'package:permission_handler/permission_handler.dart';
 import '../models/product.dart';
 import '../services/store_service.dart';
 import '../services/location_service.dart';
 import '../services/location_product_service.dart';
-
-// Énumération des directions possibles pour la navigation
+ 
 enum ArrowDirection { nord, sud, est, ouest }
-
-// Définition des événements qui peuvent être traités par le Bloc
+ 
+// Events
 abstract class NavigationEvent {}
-
+ 
 class LoadNavigationEvent extends NavigationEvent {
   final List<Product> products;
   LoadNavigationEvent({required this.products});
 }
-
+ 
 class UpdateNavigationEvent extends NavigationEvent {
   final Product product;
   UpdateNavigationEvent({required this.product});
 }
-
+ 
 class ProductFoundEvent extends NavigationEvent {
   final Product product;
   ProductFoundEvent({required this.product});
 }
-
+ 
 class UpdatePositionEvent extends NavigationEvent {
   final Product currentProduct;
   UpdatePositionEvent(this.currentProduct);
 }
-
-// Définition des états possibles du Bloc
+ 
+// States
 abstract class NavigationState {}
-
+ 
 class NavigationInitial extends NavigationState {}
-
+ 
 class NavigationLoading extends NavigationState {}
-
+ 
 class NavigationLoadedState extends NavigationState {
   final String objectName;
   final String instruction;
   final ArrowDirection arrowDirection;
   final bool isLastProduct;
-
+ 
   NavigationLoadedState({
     required this.objectName,
     required this.instruction,
@@ -55,309 +56,195 @@ class NavigationLoadedState extends NavigationState {
     this.isLastProduct = false,
   });
 }
-
+ 
 class NavigationError extends NavigationState {
   final String message;
   NavigationError(this.message);
 }
-
-// Implémentation principale du Bloc de navigation
+ 
 class NavigationBloc extends Bloc<NavigationEvent, NavigationState> {
   final StoreService _storeService;
   late final LocationService _locationService;
   late final LocationProductService _locationProductService;
-
-  // Liste des produits à trouver et index du produit courant
+  late final BluetoothScanService _bluetoothService = BluetoothScanService();
+  late StreamController<String> _dataController;
+ 
+  Timer? _navigationUpdateTimer;
   List<Product> _products = [];
   int _currentProductIndex = 0;
-
-  // Gestion des souscriptions pour le nettoyage
-  StreamSubscription? _bluetoothDataSubscription;
-  Timer? _navigationUpdateTimer;
-
+ 
   NavigationBloc(this._storeService) : super(NavigationInitial()) {
     _locationService = LocationService();
     _locationProductService = LocationProductService(_storeService);
-
-    // Enregistrement des gestionnaires d'événements
+ 
     on<LoadNavigationEvent>(_onLoadNavigation);
     on<UpdateNavigationEvent>(_onUpdateNavigation);
     on<ProductFoundEvent>(_onProductFound);
     on<UpdatePositionEvent>(_onUpdatePosition);
   }
-
-  // Gestion du chargement initial de la navigation
+ 
+  @override
+  Future<void> close() {
+    _navigationUpdateTimer?.cancel();
+    return super.close();
+  }
+ 
+  void _startNavigationUpdates(Product product) {
+    _navigationUpdateTimer?.cancel();
+    _navigationUpdateTimer = Timer.periodic(
+      const Duration(seconds: 2),
+      (_) => add(UpdatePositionEvent(product)),
+    );
+  }
+ 
   Future<void> _onLoadNavigation(
-    LoadNavigationEvent event,
-    Emitter<NavigationState> emit,
-  ) async {
-    print("DEBUG: Début de LoadNavigation");
-    emit(NavigationLoading());
-
-    try {
-      final bool permission = await checkPermissions();
-      print("DEBUG: Permissions vérifiées: $permission");
-
-      if (permission) {
-        _products = event.products;
-        _currentProductIndex = 0;
-
-        if (_products.isEmpty) {
-          emit(NavigationError('Aucun produit dans la liste'));
-          return;
-        }
-
-        // Configuration de la communication Bluetooth
-        FlutterBlue.instance.scan().listen((scanResult) {
-          if (scanResult.device.name == 'ESP32_BLE') {
-            print("DEBUG: ESP32 trouvé");
-            FlutterBlue.instance.stopScan();
-
-            scanResult.device.connect().then((_) async {
-              print("DEBUG: Connecté à l'ESP32");
-              final services = await scanResult.device.discoverServices();
-
-              await _setupBluetoothCharacteristic(services, emit);
-            });
-          }
-        });
-      } else {
-        emit(NavigationError('Permissions refusées'));
+  LoadNavigationEvent event,
+  Emitter<NavigationState> emit,
+) async {
+  final List<String> devicesList = [];
+  BluetoothDevice deviceDeMerde;
+  Map<String, dynamic> data = {};
+ 
+  try {
+    final bool permission = await checkPermissions();
+    if (permission) {
+      print("before _bluetoothService.startScan2");
+      deviceDeMerde = await _bluetoothService.startScan2();
+      if (deviceDeMerde == null) {
+        throw Exception("No device found!");
       }
-    } catch (e) {
-      print("DEBUG: Erreur globale: $e");
-      if (!emit.isDone) {
-        emit(NavigationError(e.toString()));
-      }
-    }
-  }
-
-  // Configuration des caractéristiques Bluetooth
-  Future<void> _setupBluetoothCharacteristic(
-    List<BluetoothService> services,
-    Emitter<NavigationState> emit,
-  ) async {
-    print("DEBUG: Début de _setupBluetoothCharacteristic");
-
-    // Annulation de toute souscription existante
-    await _bluetoothDataSubscription?.cancel();
-
-    for (var service in services) {
-      for (var characteristic in service.characteristics) {
-        if (characteristic.properties.notify) {
-          print("DEBUG: Caractéristique avec notification trouvée");
-          await characteristic.setNotifyValue(true);
-
-          _bluetoothDataSubscription = characteristic.value.listen(
-            (data) async {
-              print("DEBUG: Données Bluetooth reçues");
-              if (emit.isDone) {
-                await _processBluetoothData(data, emit);
-              } else {
-                print("DEBUG: Émetteur terminé, données ignorées");
-              }
-            },
-            onError: (error) {
-              print("DEBUG: Erreur dans la réception Bluetooth: $error");
-              if (!emit.isDone) {
-                emit(NavigationError(error.toString()));
-              }
-            },
-            cancelOnError: false,
-          );
-
-          print("DEBUG: Souscription Bluetooth configurée");
-        }
-      }
-    }
-    print("DEBUG: Fin de _setupBluetoothCharacteristic");
-  }
-
-  // Traitement des données reçues via Bluetooth
-  Future<void> _processBluetoothData(
-    List<int> data,
-    Emitter<NavigationState> emit,
-  ) async {
-    print("DEBUG: Début de _processBluetoothData");
-    print("DEBUG: État de emit.isDone: ${emit.isDone}");
-
-    if (emit.isDone) {
-      try {
-        print("DEBUG: Décodage des données");
-        final decodedData = utf8.decode(data);
-        print("DEBUG: Données décodées: $decodedData");
-
-        if (decodedData.isNotEmpty) {
-          print("DEBUG: Traitement des données non vides");
-          final values = decodedData.split('/');
-          print("DEBUG: Valeurs séparées: $values");
-
-          final parsedData = <String, dynamic>{};
-          for (int i = 0; i < values.length; i++) {
-            parsedData['Tag $i'] = double.tryParse(values[i]);
-          }
-          print("DEBUG: Données parsées: $parsedData");
-
-          print("DEBUG: Appel de findTargetPosition2");
-          final shortestPath = await _locationService.findTargetPosition2(
-            jsonEncode(parsedData),
-          );
-          print("DEBUG: Chemin le plus court reçu: $shortestPath");
-
-          print("DEBUG: Avant _updateNavigationWithPath");
-          print("DEBUG: État de emit.isDone avant mise à jour: ${emit.isDone}");
-          await _updateNavigationWithPath(shortestPath, emit);
-          print("DEBUG: Après _updateNavigationWithPath");
-        }
-      } catch (e) {
-        print("DEBUG: Erreur traitement données: $e");
-        if (!emit.isDone) {
-          emit(NavigationError(e.toString()));
-        }
-      }
-    } else {
-      print("DEBUG: _processBluetoothData ignoré car emit.isDone est true");
-    }
-    print("DEBUG: Fin de _processBluetoothData");
-  }
-
-  // Mise à jour de la navigation avec le nouveau chemin calculé
-  Future<void> _updateNavigationWithPath(
-    List<List<int>>? shortestPath,
-    Emitter<NavigationState> emit,
-  ) async {
-    if (emit.isDone &&
-        shortestPath != null &&
-        !listEquals(shortestPath, [
-          [-1000, -1000]
-        ])) {
-      final instruction = _generateInstruction(shortestPath);
-      final arrowDirection = _calculateDirection(shortestPath);
-
+ 
+      print("deviceDeMerde: ${deviceDeMerde}");
       emit(NavigationLoadedState(
-        objectName: _products[_currentProductIndex].name,
-        arrowDirection: arrowDirection,
-        instruction: instruction,
-        isLastProduct: _currentProductIndex == _products.length - 1,
+        objectName: "blabla",
+        instruction: "",
+        arrowDirection: ArrowDirection.nord,
+        isLastProduct: true,
       ));
-    }
-  }
+ 
+      ReceivePort receivePort = ReceivePort();
+      print("ReceivePort created");
+      print("Before spawning Isolate!!");
+      final test = await Isolate.spawn(this.test, receivePort.sendPort);
 
-  // Gestion de la mise à jour de position
-  Future<void> _onUpdatePosition(
-    UpdatePositionEvent event,
-    Emitter<NavigationState> emit,
-  ) async {
-    if (!emit.isDone) {
-      try {
-        await _updateNavigation(event.currentProduct, emit);
-      } catch (e) {
-        if (!emit.isDone) {
-          emit(NavigationError(e.toString()));
-        }
-      }
-    }
-  }
+      receivePort.listen((message) {
+        print("Message from isolate: $message");
+      });
+      // final jsonData = await Isolate.spawn(
+      //     _bluetoothService.connectToDevice, receivePort.sendPort);
+      // print("Isolate spawned");
 
-  // Gestion de la mise à jour de navigation
+      
+ 
+      // receivePort.listen((message) {
+      //   print("Message from isolate: $message");
+      //   emit(NavigationLoadedState(
+      //     objectName: "blabla",
+      //     instruction: "",
+      //     arrowDirection: ArrowDirection.nord,
+      //     isLastProduct: true,
+      //   ));
+      // });
+    }
+  } catch (e) {
+    emit(NavigationError(e.toString()));
+  }
+}
+
+void test(SendPort sendPort) async {
+    while(true){
+      sleep(new Duration(seconds: 3));
+      sendPort.send("test!!");
+    }
+}
+ 
   Future<void> _onUpdateNavigation(
     UpdateNavigationEvent event,
     Emitter<NavigationState> emit,
   ) async {
-    if (!emit.isDone) {
-      emit(NavigationLoading());
-      try {
-        await _updateNavigation(event.product, emit);
-      } catch (e) {
-        if (!emit.isDone) {
-          emit(NavigationError(e.toString()));
-        }
-      }
+    emit(NavigationLoading());
+    try {
+      await _updateNavigation(event.product, emit);
+      _startNavigationUpdates(event.product);
+    } catch (e) {
+      emit(NavigationError(e.toString()));
     }
   }
-
-  // Gestion d'un produit trouvé
+ 
   Future<void> _onProductFound(
     ProductFoundEvent event,
     Emitter<NavigationState> emit,
   ) async {
+    _navigationUpdateTimer?.cancel();
     try {
       _currentProductIndex++;
-
+ 
       if (_currentProductIndex >= _products.length) {
-        if (!emit.isDone) {
-          emit(NavigationLoadedState(
-            objectName: "Terminé !",
-            instruction: "Tous les produits ont été trouvés",
-            arrowDirection: ArrowDirection.nord,
-            isLastProduct: true,
-          ));
-        }
+        emit(NavigationLoadedState(
+          objectName: "Terminé !",
+          instruction: "Tous les produits ont été trouvés",
+          arrowDirection: ArrowDirection.nord,
+          isLastProduct: true,
+        ));
         return;
       }
-
-      if (!emit.isDone) {
-        await _updateNavigation(_products[_currentProductIndex], emit);
-      }
+ 
+      await _updateNavigation(_products[_currentProductIndex], emit);
+      _startNavigationUpdates(_products[_currentProductIndex]);
     } catch (e) {
-      if (!emit.isDone) {
-        emit(NavigationError(e.toString()));
-      }
+      emit(NavigationError(e.toString()));
     }
   }
-
-  // Mise à jour de la navigation pour un produit donné
-  Future<void> _updateNavigation(
-    Product product,
+ 
+  Future<void> _onUpdatePosition(
+    UpdatePositionEvent event,
     Emitter<NavigationState> emit,
   ) async {
-    if (emit.isDone) return;
-
     try {
-      final productPosition =
-          await _locationProductService.getProductPosition(product);
-      final currentPath =
-          await _locationService.findTargetPosition(productPosition);
-
-      if (!emit.isDone && currentPath != null && currentPath.isNotEmpty) {
-        final direction = _calculateDirection(currentPath);
-        final instruction = _generateInstruction(currentPath);
-
-        emit(NavigationLoadedState(
-          objectName: product.name,
-          arrowDirection: direction,
-          instruction: instruction,
-          isLastProduct: _currentProductIndex == _products.length - 1,
-        ));
-      } else if (!emit.isDone) {
-        emit(
-            NavigationError("Impossible de trouver un chemin vers le produit"));
-      }
+      await _updateNavigation(event.currentProduct, emit);
     } catch (e) {
-      if (!emit.isDone) {
-        emit(NavigationError(e.toString()));
-      }
+      emit(NavigationError(e.toString()));
     }
   }
-
-  // Calcul de la direction à partir du chemin
+ 
+  Future<void> _updateNavigation(
+      Product product, Emitter<NavigationState> emit) async {
+    final productPosition =
+        await _locationProductService.getProductPosition(product);
+    final currentPath =
+        await _locationService.findTargetPosition(productPosition);
+ 
+    if (currentPath != null && currentPath.isNotEmpty) {
+      final direction = _calculateDirection(currentPath);
+      final instruction = _generateInstruction(currentPath);
+ 
+      emit(NavigationLoadedState(
+        objectName: product.name,
+        arrowDirection: direction,
+        instruction: instruction,
+        isLastProduct: _currentProductIndex == _products.length - 1,
+      ));
+    } else {
+      emit(NavigationError("Impossible de trouver un chemin vers le produit"));
+    }
+  }
+ 
   ArrowDirection _calculateDirection(List<List<int>> path) {
     if (path.length < 2) return ArrowDirection.nord;
-
+ 
     final current = path[0];
     final next = path[1];
-
+ 
     if (next[0] < current[0]) return ArrowDirection.nord;
     if (next[0] > current[0]) return ArrowDirection.sud;
     if (next[1] < current[1]) return ArrowDirection.ouest;
     return ArrowDirection.est;
   }
-
-  // Génération des instructions de navigation
+ 
   String _generateInstruction(List<List<int>> path) {
     final distance = path.length - 1;
     final direction = _calculateDirection(path);
-
+ 
     switch (direction) {
       case ArrowDirection.nord:
         return "Orientation à 12H00 , Avancez de $distance pas";
@@ -369,19 +256,16 @@ class NavigationBloc extends Bloc<NavigationEvent, NavigationState> {
         return "Orientation à 9H00 , Avancez de $distance pas";
     }
   }
-
-  // Vérification des permissions nécessaires
+ 
   Future<bool> checkPermissions() async {
-    return await Permission.bluetoothScan.request().isGranted &&
+    if (await Permission.bluetoothScan.request().isGranted &&
         await Permission.bluetoothConnect.request().isGranted &&
-        await Permission.locationWhenInUse.request().isGranted;
-  }
-
-  // Nettoyage des ressources à la fermeture du Bloc
-  @override
-  Future<void> close() async {
-    await _bluetoothDataSubscription?.cancel();
-    _navigationUpdateTimer?.cancel();
-    return super.close();
+        await Permission.locationWhenInUse.request().isGranted) {
+      // if everything is OK, return true
+      return true;
+    } else {
+      // otherwise, false
+      return false;
+    }
   }
 }
--- mobile/lib/services/bluetooth_service.dart
+++ mobile/lib/services/bluetooth_service.dart
@@ -1,97 +1,211 @@
 import 'dart:async';
 import 'dart:convert';
-
+import 'dart:io';
+import 'dart:isolate';
+ 
 import 'package:flutter_blue/flutter_blue.dart';
-
+import 'package:mobile/services/shared_service.dart';
+ 
 class BluetoothScanService {
   FlutterBlue flutterBlue = FlutterBlue.instance;
   BluetoothDevice? connectedDevice;
   BluetoothCharacteristic? characteristic;
-
-  // Création d'un StreamController pour gérer les données
-  final _dataController = StreamController<Map<String, dynamic>>.broadcast();
-  Stream<Map<String, dynamic>> get dataStream => _dataController.stream;
-
-  Future<void> startScanAndConnect() async {
-    print("Début du scan Bluetooth");
-
-    try {
-      await flutterBlue.scan().firstWhere((scanResult) {
-        return scanResult.device.name == 'ESP32_BLE';
-      }).then((scanResult) async {
-        print("Appareil ESP32_BLE trouvé");
-        await flutterBlue.stopScan();
-        await _connectToDevice(scanResult.device);
-      });
-    } catch (e) {
-      print("Erreur lors du scan : $e");
-      _dataController.addError(e);
-    }
+  late SharedService sharedService;
+ 
+  // Stream for device data
+  Stream<List<int>>? dataStream;
+ 
+  // Start scanning for devices
+  void startScan(Function(BluetoothDevice) onDeviceFound) async {
+    flutterBlue.scan().listen((scanResult) {
+      if (scanResult.device.name == 'ESP32_BLE') {
+        flutterBlue.stopScan();
+        onDeviceFound(scanResult.device);
+      }
+    });
   }
-
-  Future<void> _connectToDevice(BluetoothDevice device) async {
+ 
+  /// Méthode pour se connecter à un périphérique BLE et écouter les données
+   
+void connectToDevice(SendPort sendPort) async {
+  try {
+    print("In connectToDevice!!");
+    BluetoothDevice device = await startScan2();
+    print("enter connectToDevice");
+    print("device : ${device.name}");
+    while (true){
+      sleep(new Duration(seconds: 3));
+      sendPort.send("connecToDevice");
+      }
+    }
+  catch (e){
+    print("Error connectToDevice!!!");
+    print(e);
+  }
+  //   // Connect to the device
+  //   print("Connecting to device: ${device.name}");
+  //   await device.connect();
+  //   print("after device.connect()");
+ 
+  //   // Discover services and characteristics
+  //   print("Discovering services...");
+  //   List<BluetoothService> services = await device.discoverServices();
+  //   for (var service in services) {
+  //     for (var characteristic in service.characteristics) {
+  //       // If a characteristic allows notification
+  //       if (characteristic.properties.notify) {
+  //         print("Characteristic found with notifications: ${characteristic.uuid}");
+  //         await characteristic.setNotifyValue(true);
+  //         Stream<List<int>> dataStream = characteristic.value.asBroadcastStream();
+ 
+  //         // Listen to data sent by the ESP32
+  //         dataStream.listen((data) async {
+  //           try {
+  //             print("Data received (bytes): $data");
+ 
+  //             // Decode the received data to text
+  //             String decodedData = utf8.decode(data);
+  //             print("Data received (text): $decodedData");
+  //             sendPort.send(decodedData);
+ 
+  //             if (decodedData.isNotEmpty) {
+  //               try {
+  //                 // Process data as numbers separated by "/"
+  //                 List<String> values = decodedData.split('/');
+  //                 Map<String, dynamic> parsedData = {};
+ 
+  //                 for (int i = 0; i < values.length; i++) {
+  //                   parsedData['Tag $i'] = double.tryParse(values[i]);
+  //                 }
+ 
+  //                 print("Parsed data: $parsedData");
+  //               } catch (e) {
+  //                 print("Error processing data: $e");
+  //               }
+  //             }
+  //           } catch (e) {
+  //             print("Error decoding data: $e");
+  //           }
+  //         });
+  //       }
+  //     }
+  //   }
+  // } catch (e) {
+  //   print("Error connecting to device: $e");
+  // }
+}
+ 
+  Future<String> connectToDeviceString(BluetoothDevice device) async {
     try {
+      // Connexion au périphérique
       print("Connexion au périphérique : ${device.name}");
       await device.connect();
       connectedDevice = device;
-
+ 
+      // Découverte des services et caractéristiques
       print("Découverte des services...");
-      final services = await device.discoverServices();
-
+      List<BluetoothService> services = await device.discoverServices();
+ 
       for (var service in services) {
         for (var characteristic in service.characteristics) {
+          // Si une caractéristique permet la notification
           if (characteristic.properties.notify) {
             print(
-                "Caractéristique avec notifications trouvée : ${characteristic.uuid}");
+                "Caractéristique trouvée avec notifications : ${characteristic.uuid}");
             await characteristic.setNotifyValue(true);
-
-            characteristic.value.listen((data) {
-              _processData(data);
-            }, onError: (error) {
-              print("Erreur de réception des données : $error");
-              _dataController.addError(error);
-            });
+            dataStream = characteristic.value.asBroadcastStream();
+ 
+            // Écoute des données envoyées par l'ESP32
+            await for (var data in dataStream!) {
+              try {
+                print("Données reçues (bytes) : $data");
+ 
+                // Décodage des données reçues en texte
+                String decodedData = utf8.decode(data);
+                print("Données reçues (texte) : $decodedData");
+ 
+                if (decodedData.isNotEmpty) {
+                  try {
+                    // Traitement des données sous forme de nombres séparés par "/"
+                    List<String> values = decodedData.split('/');
+                    Map<String, dynamic> parsedData = {};
+ 
+                    for (int i = 0; i < values.length; i++) {
+                      parsedData['Tag $i'] = double.tryParse(values[i]);
+                    }
+ 
+                    print("Données parsées : $parsedData");
+ 
+                    // Retourner les données traitées sous forme de chaîne
+                    return jsonEncode(
+                        parsedData); // Vous pouvez modifier ici pour renvoyer une chaîne spécifique
+                  } catch (e) {
+                    print("Erreur lors du traitement des données : $e");
+                    return "Erreur lors du traitement des données.";
+                  }
+                } else {
+                  connectToDeviceString(device);
+                  return "Aucune donnée reçue";
+                }
+              } catch (e) {
+                print("Erreur lors du décodage des données : $e");
+                return "Erreur lors du décodage des données.";
+              }
+            }
           }
         }
       }
+      return "Aucune caractéristique avec notifications trouvée.";
     } catch (e) {
-      print("Erreur de connexion : $e");
-      _dataController.addError(e);
+      print("Erreur lors de la connexion : $e");
+      return "Erreur lors de la connexion : $e";
     }
   }
+ 
+  Future<BluetoothDevice> startScan2() async {
+    print("startScan2 FUTURE");
+    // Créez une instance de FlutterBlue pour le scan
+    FlutterBlue flutterBlue = FlutterBlue.instance;
+    print("startScan2 flutterBlue");
+ 
+    // Lancez un scan pour trouver les appareils
+    final completer = Completer<BluetoothDevice>(); // Création du Completer
+    print("startScan2 completer");
 
-  void _processData(List<int> data) {
-    try {
-      final decodedData = utf8.decode(data);
-      print("Données décodées : $decodedData");
-
-      if (decodedData.isNotEmpty) {
-        final values = decodedData.split('/');
-        final parsedData = <String, dynamic>{};
-
-        for (int i = 0; i < values.length; i++) {
-          parsedData['Tag $i'] = double.tryParse(values[i]);
-        }
-
-        print("Données traitées : $parsedData");
-        _dataController.add(parsedData);
+  if (await flutterBlue.isScanning.first) {
+    await flutterBlue.stopScan();
+  }
+ 
+    // Écoutez les résultats du scan
+    var scanSubscription = flutterBlue.scan().listen((scanResult) {
+      print("startScan2 scanSubscription");
+      // Lorsque le périphérique souhaité est trouvé
+      if (scanResult.device.name == 'ESP32_BLE') {
+        // Arrêtez le scan une fois que l'appareil est trouvé
+        flutterBlue.stopScan();
+ 
+        // Retournez le périphérique trouvé
+        completer.complete(scanResult
+            .device); // Complétez le Future avec le périphérique trouvé
       }
-    } catch (e) {
-      print("Erreur de traitement des données : $e");
-      _dataController.addError(e);
+    });
+ 
+    // Si le scan prend trop de temps sans trouver l'appareil, on peut gérer un timeout
+    await Future.delayed(const Duration(seconds: 10));
+ 
+    // Si l'appareil n'a pas été trouvé, on annule le scan
+    if (!completer.isCompleted) {
+      flutterBlue.stopScan();
+      completer.completeError(
+          'Device not found'); // Retourner une erreur si le périphérique n'est pas trouvé
     }
+ 
+    // Attendez et retournez le périphérique trouvé
+    return completer.future;
   }
-
-  Future<void> disconnect() async {
-    try {
-      await connectedDevice?.disconnect();
-      await _dataController.close();
-    } catch (e) {
-      print("Erreur lors de la déconnexion : $e");
-    }
-  }
-
-  void dispose() {
-    disconnect();
+ 
+  // Disconnect from device
+  void disconnect() {
+    connectedDevice?.disconnect();
   }
 }
