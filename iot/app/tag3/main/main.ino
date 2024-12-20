ce2ad1747be5d78b46a68fd2875d140ae5e97678
Aurélien_SCHMIT <aurelien.schmit@epitech.eu>
2024-12-20 16:07:46 +0100

    fix(io): id tag3 fixed

--- iot/app/tag3/main/main.ino
+++ iot/app/tag3/main/main.ino
@@ -1,7 +1,7 @@
 #include <SPI.h>
 #include "DW1000Ranging.h"
 
-#define TAG_ADDRESS "5A:44:45:64:5D:C2:3A:9B"
+#define TAG_ADDRESS "9E:11:33:BC:72:81:3C:9D"
 #define SPI_SCK 18
 #define SPI_MISO 19
 #define SPI_MOSI 23
@@ -11,7 +11,8 @@ const uint8_t PIN_RST = 27; // reset pin
 const uint8_t PIN_IRQ = 34; // irq pin
 const uint8_t PIN_SS = 4;   // spi select pin
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     delay(1000);
     Serial.println("Starting as TAG...");
@@ -22,6 +23,7 @@ void setup() {
     Serial.println("TAG initialized and running...");
 }
 
-void loop() {
+void loop()
+{
     DW1000Ranging.loop();
 }
