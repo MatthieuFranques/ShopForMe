c21f66987c9b2daa813e9921f4ff921398e2c6c3
Corentin DEPRECQ <corentin.deprecq@epitech.eu>
2025-04-10 17:04:37 +0200

    feat(front): demo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Login from "./components/Login/Login";
 import NavBar from "./components/NavBar/NavBar";
 import Shop from "./components/Shop/Shop";
 import SingleShop from "./components/Shop/SingleShop";
+import Demo from "./components/Demo/Demo";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                     <Route path="/login" element={<Login />} />
                     <Route path="/" element={<Accueil />} />
                     <Route path="/shop" element={<Shop />} />
+                    <Route path="/demo" element={<Demo />} />
                     <Route path="/shop/:id" element={<SingleShop />} />
                 </Routes>
             </BrowserRouter>
